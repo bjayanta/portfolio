1fbf8fbabb6f27957129e99712b1ace61ee28063
Jayanta <bjayanta.me@gmail.com>
2024-05-29 11:52:26 +0600

    start my-experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
 					</h2>
 				</div>
 
-				<div className="container grid grid-cols-5 gap-6">
+				<div className="container grid grid-cols-5 gap-6 mb-24">
 					<figure className="flex flex-col justify-center items-center space-y-6 border-2 border-black py-10 rounded hover:bg-black hover:text-white">
 						<FaJs size={64} />
 						<figcaption className="text-xl font-bold">JavaScript</figcaption>
@@ -150,8 +150,112 @@ export default function Home() {
 				</div>
 			</section>
 
-			
+			{/* My Experience */}
+			<section className="bg-black text-white">
+				<div className="container text-center py-16">
+					<h2 className="text-5xl">
+						<span>My</span> {' '}
+						<span className="font-black">Experience</span>
+					</h2>
+				</div>
+
+				<div className="container space-y-6 mb-24">
+					{/* AmharcTech */}
+					<article className="border border-zinc-600 rounded-lg p-12 hover:bg-zinc-800">
+						<div className="flex items-center justify-between">
+							<figure className="flex items-center space-x-6 mb-4">
+								<Link href="https://amharctech.com/" target="_blank">
+									<Image src="/images/amharctech.png" width={32} height={32} alt="AmharcTech" />
+								</Link>
+								
+								<figcaption className="text-2xl font-semibold">
+									Full-stack web developer at AmharcTech
+								</figcaption>
+							</figure>
+							
+							<p className="text-zinc-300">February 2023 - Present</p>
+						</div>
+
+						<p className="text-zinc-300">As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google&lsquo;s core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p>
+					</article>
+
+					{/* MaxSOP */}
+					<article className="border border-zinc-600 rounded-lg p-12 hover:bg-zinc-800">
+						<div className="flex items-center justify-between">
+							<figure className="flex items-center space-x-6 mb-4">
+								<Link href="https://maxsop.com/" target="_blank">
+									<Image src="/images/maxsop.jpg" width={32} height={32} alt="MaxSOP" />
+								</Link>
+								
+								<figcaption className="text-2xl font-semibold">
+									Senior software developer at MaxSOP
+								</figcaption>
+							</figure>
+							
+							<p className="text-zinc-300">September 2019 - January 2023</p>
+						</div>
+
+						<p className="text-zinc-300">As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google&lsquo;s core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p>
+					</article>
+
+					{/* USITSolution */}
+					<article className="border border-zinc-600 rounded-lg p-12 hover:bg-zinc-800">
+						<div className="flex items-center justify-between">
+							<figure className="flex items-center space-x-6 mb-4">
+								<Link href="https://usitsolution.net/" target="_blank">
+									<Image src="/images/usitsolution.png" width={32} height={32} alt="USITSolution" />
+								</Link>
+								
+								<figcaption className="text-2xl font-semibold">
+									Senior software developer at USITSolution
+								</figcaption>
+							</figure>
+							
+							<p className="text-zinc-300">February 2018 - September 2019</p>
+						</div>
+
+						<p className="text-zinc-300">As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google&lsquo;s core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p>
+					</article>
+
+					{/* Freelance IT Lab */}
+					<article className="border border-zinc-600 rounded-lg p-12 hover:bg-zinc-800">
+						<div className="flex items-center justify-between">
+							<figure className="flex items-center space-x-6 mb-4">
+								<Link href="https://www.freelanceit.com.bd/" target="_blank">
+									<Image src="/images/freelanceitlab.png" width={32} height={32} alt="Freelance IT Lab" />
+								</Link>
+								
+								<figcaption className="text-2xl font-semibold">
+									Senior software developer at Freelance IT Lab
+								</figcaption>
+							</figure>
+							
+							<p className="text-zinc-300">February 2015 - January 2018</p>
+						</div>
+
+						<p className="text-zinc-300">As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google&lsquo;s core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p>
+					</article>
+
+					{/* Freelance IT Lab */}
+					<article className="border border-zinc-600 rounded-lg p-12 hover:bg-zinc-800">
+						<div className="flex items-center justify-between">
+							<figure className="flex items-center space-x-6 mb-4">
+								<Link href="https://www.freelanceit.com.bd/" target="_blank">
+									<Image src="/images/freelanceitlab.png" width={32} height={32} alt="Freelance IT Lab" />
+								</Link>
+								
+								<figcaption className="text-2xl font-semibold">
+									Junior software developer at Freelance IT Lab
+								</figcaption>
+							</figure>
+							
+							<p className="text-zinc-300">August 2013 - January 2015</p>
+						</div>
+
+						<p className="text-zinc-300">As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google&lsquo;s core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p>
+					</article>
+				</div>
+			</section>
 		</main>
 	);
 }
-
